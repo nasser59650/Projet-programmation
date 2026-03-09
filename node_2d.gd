0ec42be85a59d10e37b15a078319f32619414d17
nasser59650 <nasser59650@gmail.com>
2026-03-09 22:20:01 +0100

    Ajout boutique + jauge vitesse

--- node_2d.gd
+++ node_2d.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @export var pieton_scene = preload("res://piéton_1.tscn")
 var spawn_timer = 0.0
-const SPAWN_TIME = 10.0
+const SPAWN_TIME = 3.0
 
 func _process(delta):
 	spawn_timer += delta
@@ -13,7 +13,7 @@ func _process(delta):
 
 func _spawn_pieton():
 	var pieton = pieton_scene.instantiate()
-	pieton.position = Vector2(randi() % 500, randi() % 500)
+	pieton.position = Vector2(-2280 + randi() % 3262, -2088 + randi() % 3275)
 	add_child(pieton)
 
 
