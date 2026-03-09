7d71652d9faad00d31fcc84fa4c9ae3a798d7bc2
nasser59650 <nasser59650@gmail.com>
2026-03-09 17:20:27 +0100

    Ajout arrêt automatique piétons

--- node_2d.gd
+++ node_2d.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 @export var pieton_scene = preload("res://piéton_1.tscn")
 var spawn_timer = 0.0
-const SPAWN_TIME = 5.0
+const SPAWN_TIME = 10.0
 
 func _process(delta):
 	spawn_timer += delta
@@ -15,3 +15,9 @@ func _spawn_pieton():
 	var pieton = pieton_scene.instantiate()
 	pieton.position = Vector2(randi() % 500, randi() % 500)
 	add_child(pieton)
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	print("body entered: ", body.name)
+	if body is CharacterBody2D:
+		body.entrer_route()
