bb058224689cf2785e639ef70f8a4ec43b9edc86
nasser59650 <nasser59650@gmail.com>
2026-03-10 17:19:30 +0100

    ajout 2 voitures

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 var dezoom = false
 const SPEED = 100.0
-const BOOST_SPEED = 200.0
+const BOOST_SPEED = 800.0
 var boost_energy = 100.0
 const BOOST_MAX = 100.0
 const BOOST_DRAIN = 30.0
--- node_2d.gd
+++ node_2d.gd
@@ -1,12 +1,17 @@
 extends Node2D
 
 @export var pieton_scene = preload("res://piéton_1.tscn")
+@export var voiture1_scene = preload("res://voiture_principale.tscn")
+@export var voiture2_scene = preload("res://voiture_principale2.tscn")
+
 var spawn_timer = 0.0
 const SPAWN_TIME = 3.0
+var voiture_actuelle = null
 
 func _ready():
 	for i in range(50):
 		_spawn_pieton()
+	voiture_actuelle = get_node("voiture principale")
 
 func _process(delta):
 	spawn_timer += delta
@@ -14,14 +19,23 @@ func _process(delta):
 	if spawn_timer >= SPAWN_TIME:
 		spawn_timer = 0.0
 		_spawn_pieton()
+	
+	if Input.is_action_just_pressed("changer_voiture"):
+		changer_voiture(voiture2_scene)
 
 func _spawn_pieton():
 	var pieton = pieton_scene.instantiate()
 	pieton.position = Vector2(-2280 + randi() % 3262, -2088 + randi() % 3275)
 	add_child(pieton)
 
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("body entered: ", body.name)
 	if body is CharacterBody2D:
 		body.entrer_route()
+
+func changer_voiture(nouvelle_scene):
+	var pos = voiture_actuelle.position
+	voiture_actuelle.queue_free()
+	var nouvelle_voiture = nouvelle_scene.instantiate()
+	nouvelle_voiture.position = pos
+	add_child(nouvelle_voiture)
+	voiture_actuelle = nouvelle_voiture
