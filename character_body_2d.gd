3ae45c806b18e1b81dd904f98414dd06c3c15886
nasser59650 <nasser59650@gmail.com>
2026-03-10 16:41:58 +0100

    20 piétons pour perdre

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -69,5 +69,5 @@ func _on_bouton_boost_pressed() -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("pietons"):
-		compteur += 2
+		compteur += 5
 		body.queue_free()
